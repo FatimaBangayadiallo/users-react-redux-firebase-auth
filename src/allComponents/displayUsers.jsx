27bbfd7fefd35ad137b429866d4e9765e8ea19d2
bangaya diallo <bangayadiall@gmail.com>
2021-11-17 12:22:10 +0000

    the setup

---
+++ src/allComponents/displayUsers.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import User from "./user";
+const DisplayUsers = () => {
+  return (
+    <>
+      je suis DisplayUsers
+      <User />
+    </>
+  );
+};
+export default DisplayUsers;
---
+++ src/allComponents/user.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const User = () => {
+  return <>one user prototype</>;
+};
+export default User;
